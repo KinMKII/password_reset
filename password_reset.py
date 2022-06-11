6ecab504ce54ae617f3c6ec1084b8834e36eca19
Kin <kins2010@gmail.com>
2022-06-11 19:41:51 +0800

    change to while x > 0

--- password_reset.py
+++ password_reset.py
@@ -1,13 +1,11 @@
 password = 'a123456' #設定密碼
-x = 3    #可錯誤次數
+x = 3 #可錯誤次數
 
-while True:
+while x > 0:
     input_password = input('請輸入密碼: ')
     if input_password == password:
         print('登入成功!')
-        break    #逃出迴圈
+        break #逃出迴圈
     else:
         x = x - 1
         print('密碼錯誤! 還有', x , '次機會')
-        if x == 0:
-            break
