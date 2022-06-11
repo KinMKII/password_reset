45884da4eacc64562a17db18e632c3c1e50521aa
Kin <kins2010@gmail.com>
2022-06-11 19:35:17 +0800

    3rd modify

--- password_reset.py
+++ password_reset.py
@@ -7,21 +7,7 @@ while True:
         print('登入成功!')
         break
     else:
-        x = x- 1
+        x = x - 1
         print('密碼錯誤! 還有', x , '次機會')
-        input_password = input('請輸入密碼: ')
-        if input_password == password:
-            print('登入成功!')
+        if x == 0:
             break
-        else:
-            x = x- 1
-            print('密碼錯誤! 還有', x , '次機會')
-            input_password = input('請輸入密碼: ')
-            if input_password == password:
-                print('登入成功!')
-                break
-            else:
-                x = x- 1
-                print('密碼錯誤! 還有', x , '次機會')
-                break
-    break
