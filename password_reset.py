cd717d7ffbfa0b5b451a7ed73008288500031839
Kin <kins2010@gmail.com>
2022-06-11 19:38:06 +0800

    3rd modify with notes

--- password_reset.py
+++ password_reset.py
@@ -1,11 +1,11 @@
-password = 'a123456'
-x = 3
+password = 'a123456' #設定密碼
+x = 3    #可錯誤次數
 
 while True:
     input_password = input('請輸入密碼: ')
     if input_password == password:
         print('登入成功!')
-        break
+        break    #逃出迴圈
     else:
         x = x - 1
         print('密碼錯誤! 還有', x , '次機會')
